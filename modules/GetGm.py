3268d4b7ad10db4557a07876fbeb718376419d5e
1dimadim1 <1dimadim1@gmail.com>
2023-04-26 01:44:28 +0300

    feat: added getGM

---
+++ modules/GetGm.py
@@ -0,0 +1,14 @@
+from pycalphad import Database, calculate
+import numpy as np
+
+
+def getGM(db, components, all_phases,possible_phases, amounts, T, P = 101325):
+    results = [np.nan] * len(all_phases)
+    for i in range(len(all_phases)):
+        if all_phases[i] in possible_phases:
+            try:
+                values = calculate(db, components, all_phases[i] , output = 'GM', P = P, T = T, points = amounts)
+                results[i] = values.GM.values.flat[0]
+            except Exception as e:
+                continue
+    return results
